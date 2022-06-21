5708f55735b68a73c516d18810eb5014143c5214
KyungMin0308 <km3462@naver.com>
2022-06-21 15:23:50 +0900

    프로젝트: 프로젝트 생성

---
+++ src/main/java/com/kyungmin/grademanagement/GrademanagementApplication.java
@@ -0,0 +1,13 @@
+package com.kyungmin.grademanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrademanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrademanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyungmin/grademanagement/GrademanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyungmin.grademanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrademanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
