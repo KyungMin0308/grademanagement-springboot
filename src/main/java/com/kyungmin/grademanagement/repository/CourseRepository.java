7bd7d61d42f48307b8d8e1f36da1cef2a68f2a95
KyungMin0308 <km3462@naver.com>
2022-07-09 22:19:22 +0900

    기능추가: 해당 년도와 학기에 수강한 과목 조회 기능 추가 및 테스트 코드 작성
    CourseRepository에 년도(year)와 학기(semester)를 기준으로 해당 학기 전체 과목을 조회하는 기능을 수행할 함수 findByYearAndSemester() 작성
    
    함수가 제대로 동작하는지 확인하기 위해 테스트 코드에 getCourseByYearAndSemester() 함수를 작성하고 2016년도 1학기 과목을 조회하여 콘솔에 출력하여 테스트

--- src/main/java/com/kyungmin/grademanagement/repository/CourseRepository.java
+++ src/main/java/com/kyungmin/grademanagement/repository/CourseRepository.java
@@ -5,6 +5,8 @@ import com.kyungmin.grademanagement.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 /*
 기능 목록(CRUD)
   1. Create
@@ -22,15 +24,20 @@ import org.springframework.data.jpa.repository.Query;
 */
 public interface CourseRepository extends JpaRepository<Course, Integer> { //DAO 클래스
 
-    //과목명으로 조회(Update에서 사용)
-    Course findBySubject(String subject);
-
-    //수강년도, 학기를 기준으로 수강년도, 학기, 이수 학점을 조회(Read에서 사용)
+    //Read
+    //1)수강년도, 학기를 기준으로 수강년도, 학기, 총 이수 학점을 조회
     @Query(value="select c.year AS year, c.semester AS semester, sum(c.credit) AS credit " +
             "from Course_Info c " +
             "where c.year = :year and c.semester = :semester " +
             "group by c.year, c.semester"
             , nativeQuery = true)
     YearAndSemesterProjection findTotalCreditByYearAndSemester(int year, int semester);
+
+    //2)수강년도, 학기 기준 해당 학기 전체 과목 조회
+    List<Course> findByYearAndSemester(int year, int semester);
+
+    //Update
+    //1)과목명으로 Course 조회
+    Course findBySubject(String subject);
 
 }
--- src/test/java/com/kyungmin/grademanagement/CourseRepositoryTest.java
+++ src/test/java/com/kyungmin/grademanagement/CourseRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.kyungmin.grademanagement;
 
 import com.kyungmin.grademanagement.dto.YearAndSemesterProjection;
+import com.kyungmin.grademanagement.model.Course;
 import com.kyungmin.grademanagement.repository.CourseRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,12 +18,20 @@ public class CourseRepositoryTest {
     @Autowired
     CourseRepository courseRepository;
 
-    //DB에서 데이터를 제대로 읽어오는지 확인
     @Test
-    public void getSemesterCourse() {
+    public void getTotalCreditByYearAndSemester() { //수강년도, 학기별 총 이수학점 조회
         YearAndSemesterProjection ysp = courseRepository.findTotalCreditByYearAndSemester(2016, 1);
         System.out.println(ysp.getYear());
         System.out.println(ysp.getSemester());
         System.out.println(ysp.getCredit());
+    }
+
+    @Test
+    public void getCourseByYearAndSemester() { //해당 년도와 학기에 수강한 과목 정보 조회
+        List<Course> courseList = courseRepository.findByYearAndSemester(2016, 1);
+        for(Course course: courseList) {
+            System.out.println(course.getYear() + " " + course.getSemester() + " " + course.getSubject() + " "
+                    + course.getCurriculum() + " " + course.getCredit() + " " + course.getGrade());
+        }
     }
 }
